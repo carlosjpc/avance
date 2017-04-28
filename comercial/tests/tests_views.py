881a96e3e104f728c107f3a6df2ea3360131638d
carlosjpc <carlosjpc87@gmail.com>
2017-04-27 21:44:54 -0500

    crear una carpeta para los tests, divididos por views, forms..

--- comercial/tests/tests_views.py
+++ comercial/tests/tests_views.py
@@ -7,18 +7,19 @@ from comercial.models import (Cliente, Contacto_C, Direccion_Fiscal_Cliente, Age
 # Create your tests here.
 class AgenciaTestCase(TestCase):
     def setUp(self):
+        comercial_v = Group.objects.create(name='comercial_vendedor')
         Claudio_V = User.objects.create_user(username='Claudio_V', first_name='Claudio', last_name='Velazquez',
                                         email='claudio@avance.mg', password="shjshjsh")
+        Claudio_V.groups.add(comercial_v)
         Fernanda_R = User.objects.create_user(username='Fernanda_R', first_name='Fernanda', last_name='Rivera',
                                         email='fernanda@avance.mg', password="bsghak")
+        Fernanda_R.groups.add(comercial_v)
 
         Agencia_Automotriz.objects.create(Marca='MZ', Grupo='al', Ciudad="CDMX", Colonia="Tacubaya",
                                         Telefono="55438789", Atiende=Claudio_V)
         Agencia_Automotriz.objects.create(Marca='KI', Grupo='al', Ciudad="Queretaro", Colonia="San Juan",
                                         Telefono="431 438 7789", Atiende=Fernanda_R)
 
-        Cliente.objects.create(RFC='ACL9807HJMF6T', Persona_Moral=True, Nombre_Empresa='Arroz con Leche',
-                                Tipo_Negocio='FA', Industria='Textil', Atiende=Claudio_V, Status='PR')
 
     def test_lista_agencias(self):
         self.client.login(username='Claudio_V', password='shjshjsh')
@@ -28,20 +29,39 @@ class AgenciaTestCase(TestCase):
         self.assertContains(response, 'Tacubaya')
         self.assertContains(response, 'Queretaro')
 
+    def test_detalle_agencia(self):
+        self.client.login(username='Claudio_V', password='shjshjsh')
+        response = self.client.get(reverse('detalle_agencia', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'comercial/detalle_agencia.html')
+        self.assertContains(response, 'Tacubaya')
+
 class ClienteTestCase(TestCase):
     def setUp(self):
+        comercial_v = Group.objects.create(name='comercial_vendedor')
         Claudio_V = User.objects.create_user(username='Claudio_V', first_name='Claudio', last_name='Velazquez',
                                         email='claudio@avance.mg', password="shjshjsh")
+        Claudio_V.groups.add(comercial_v)
+        Fernanda_R = User.objects.create_user(username='Fernanda_R', first_name='Fernanda', last_name='Rivera',
+                                        email='fernanda@avance.mg', password="bsghak")
+        Fernanda_R.groups.add(comercial_v)
 
         Cliente.objects.create(RFC='ACL9807HJMF6T', Persona_Moral=True, Nombre_Empresa='Arroz con Leche',
                                 Tipo_Negocio='FA', Industria='Textil', Atiende=Claudio_V, Status='PR')
 
+    #vendedor puede revisar los detalles de su cliente
     def test_detalle_cliente(self):
         self.client.login(username='Claudio_V', password='shjshjsh')
         response = self.client.get(reverse('detalle_cliente', kwargs={'pk': 1}))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'comercial/detalle_cliente.html')
         self.assertContains(response, 'Arroz con Leche')
+
+    #vendedor no puede revisar los detalles del cliente de otro vendedor
+    def test_vista_denegada(self):
+        self.client.login(username='Fernanda_R', password='bsghak')
+        response = self.client.get(reverse('detalle_cliente', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 404)
 
 class Lista_ClienteTestCase(TestCase):
     def setUp(self):
